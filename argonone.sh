1d5f582e69c8c0174a6e4aa7540f48737c28b519
Cai Lin <80376532+anime-in-the-world@users.noreply.github.com>
2023-09-22 23:56:58 +0600

    fixes

--- argonone.sh
+++ argonone.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-pacman -S i2c-tools gcc dtc make git bash 
+USER_HOME=$(eval echo ~${SUDO_USER})
+# pacman -S i2c-tools gcc dtc make git bash 
 i2cdetect -y 1
-cd ~/Downloads/argononed
+cd ${USER_HOME}/Downloads/argonone-fan-arch-linux
 make mrproper
 ./configure
 argonone-cli --fan0 100
 argonone-cli --temp0 30
 argonone-cli --hysteresis 3
 argonone-cli --commit
-cd
+cd ${USER_HOME}
 echo 'Fan is probably running if everything went okay'
-echo "alias argonone='sudo bash ~/Downloads/argonone-fan-arch-linux/argonone.sh" >> ~/.bashrc
+echo alias argonone='sudo bash ~/Downloads/argonone-fan-arch-linux/argonone.sh' >> ${USER_HOME}/.bashrc
